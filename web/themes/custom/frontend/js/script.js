972b4f69ca9359c5d3f8ee0c36293349e3110fdb
Stefan Butura <stefanbutura9@gmail.com>
2022-08-01 19:21:15 +0300

    Add missing script.js file

---
+++ web/themes/custom/frontend/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * ICCWC Frontend JS.
+ */
+
+(function ($, Drupal) {
+
+  /**
+   * ICCWC behavior.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.iccwc = {
+    attach(context, settings) {
+
+    },
+  };
+})(jQuery, Drupal);
