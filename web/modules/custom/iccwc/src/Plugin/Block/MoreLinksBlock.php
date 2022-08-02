117b2aac7238d23d3559ffe7afaab73f1f5d3c6a
paul <paul.martinas@s.unibuc.ro>
2022-08-02 16:54:29 +0300

    refs #14770 - create a more links block

---
+++ web/modules/custom/iccwc/src/Plugin/Block/MoreLinksBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\iccwc\Plugin\Block;
+
+use Drupal;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'ICCWC more links' Block.
+ *
+ * @Block(
+ *   id = "iccwc_more_links",
+ *   admin_label = @Translation("ICCWC more links block"),
+ *   category = @Translation("ICCWC"),
+ * )
+ */
+class MoreLinksBlock  extends BlockBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+/** @var Drupal\node\Entity\Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $links = [];
+    foreach ($node->get('field_links') as $item) {
+      $links[$item->uri] = $item->title;
+    }
+
+    return [
+      '#theme' => 'more_links',
+      '#links' => $links
+    ];
+  }
+
+}
