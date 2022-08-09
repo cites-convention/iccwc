944850d66ac3296ca1c4f48ee1e770c20afa75a7
Stefan Butura <stefanbutura9@gmail.com>
2022-08-09 13:42:17 +0300

    Work on sidebar menu

---
+++ web/modules/custom/iccwc/src/Plugin/Block/SidebarMenuBlock.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\iccwc\Plugin\Block;
+
+use Drupal\Core\Menu\MenuTreeParameters;
+use Drupal\system\Plugin\Block\SystemMenuBlock;
+
+/**
+ * Provides a 'Sidebar menu' Block.
+ *
+ * @Block(
+ *   id = "sidebar_menu",
+ *   admin_label = @Translation("Sidebar menu"),
+ *   category = @Translation("ICCWC"),
+ *   deriver = "Drupal\system\Plugin\Derivative\SystemMenuBlock",
+ *   forms = {
+ *     "settings_tray" = "\Drupal\system\Form\SystemMenuOffCanvasForm",
+ *   },
+ * )
+ */
+class SidebarMenuBlock extends SystemMenuBlock {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = parent::build();
+    $build['#theme'] = 'menu__main__sidebar';
+    return $build;
+  }
+
+}
