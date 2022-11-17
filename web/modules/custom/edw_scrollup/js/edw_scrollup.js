bb29481c490793573e18e0f7081bf9a1f3e554c0
Stefan Butura <stefanbutura9@gmail.com>
2022-11-17 19:31:35 +0200

    Lazyload twitter timeline

--- web/modules/custom/edw_scrollup/js/edw_scrollup.js
+++ web/modules/custom/edw_scrollup/js/edw_scrollup.js
@@ -4,6 +4,27 @@
 
     attach: function (context, settings) {
       $(document).ready(function(){
+        $.event.special.touchstart = {
+          setup: function( _, ns, handle ) {
+            this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+          }
+        };
+        $.event.special.touchmove = {
+          setup: function( _, ns, handle ) {
+            this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+          }
+        };
+        $.event.special.wheel = {
+          setup: function( _, ns, handle ){
+            this.addEventListener("wheel", handle, { passive: true });
+          }
+        };
+        $.event.special.mousewheel = {
+          setup: function( _, ns, handle ){
+            this.addEventListener("mousewheel", handle, { passive: true });
+          }
+        };
+
         if(drupalSettings.edw_scrollup_title == '' || drupalSettings.edw_scrollup_title == null){
           var scroll_title = '';
         } else {
--- web/themes/custom/frontend/src/js/script.js
+++ web/themes/custom/frontend/src/js/script.js
@@ -12,6 +12,20 @@
    */
   Drupal.behaviors.iccwc = {
     attach(context, settings) {
+      // Lazy load twitter timeline.
+      if ($('.twitter-timeline').length > 0) {
+        let $window = $(window).on('scroll', function loadTwitterTimeline(e){
+          var element_position = $('.twitter-timeline').offset().top;
+          var y_scroll_pos = window.pageYOffset + window.innerHeight;
+
+          if (y_scroll_pos > element_position - 700) {
+            var element = document.createElement("script");
+            element.src = "//platform.twitter.com/widgets.js";
+            document.body.appendChild(element);
+            $window.off('scroll', loadTwitterTimeline);
+          }
+        });
+      }
     },
   };
 })(jQuery, Drupal);
