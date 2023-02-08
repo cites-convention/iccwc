22cd8fdc2eae2c935681292ed33412daf9c073fd
Andrei Bautu <abautu@gmail.com>
2023-02-08 12:17:06 +0200

    Infra setup

--- commands.sh
+++ commands.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Running commands before entrypoint"
+set -x
+
+cd /var/www/drupal/
+
+cp deploy/prod.robo.yml robo.yml
+./vendor/bin/robo site:update
+
+if [ "$ENV" != "prod" ]
+then
+  echo -e "User-agent: *\nDisallow: /" > web/robots.txt
+fi
+
+date -Iseconds > 'web/deployment.html'
